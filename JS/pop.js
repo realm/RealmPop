49c753057952e20b8b6f0e47d22ce8983d060091
Marin Todorov <marin@underplot.com>
2017-02-07 15:57:40 +0100

    fixes indentation

--- JS/pop.js
+++ JS/pop.js
@@ -2,7 +2,7 @@
 
 //
 // TODO: 1. replace with your own admin token before starting the app
-// 2. replace the server IP 
+// 2. replace the server IP
 //
 
 const REALM_ADMIN_TOKEN = "ewoJImlkZW50aXR5IjogIl9fYXV0aCIsCgkiYWNjZXNzIjogWyJ1cGxvYWQiLCAiZG93bmxvYWQiLCAibWFuYWdlIl0KfQo=:JrlE/EenpDV8n8a6UZd3ybmIEsVobO+eblIuVT+g+GEzgX1JSXmMe5qDot6TapEshQkq5k2NUGsxXKug8eIW277ijjN9Vh7teJbau9HykkUQHGG238kdBn+jlUhJbgRy5wQ7Om0ft/A9qSxtueYoEGXZtJZHFtvs/6dIoK7KuVSy1BMsh19b6164z2ZX8dZv/kqQRbC4luU/9HbK1IGyUevD9pgS8Am010PC9dRimfO1mdxekgTTCaaYAVicWmIEw44wNrNOMKVzJg0xnv9VtCTdDBLPdcDc8JEOQPuaaThO2eGCBCKJQMD//WtFf2z9IfTFeRRc7Jrfl8o4PKOH7Q==";
@@ -23,42 +23,42 @@ function isRealmObject(x) {
  */
 
 function Pop(url, path) {
-  this.url = url;
-  this.path = path;
+    this.url = url;
+    this.path = path;
 }
 
 Pop.prototype.connect = function(token) {
-  var admin = Realm.Sync.User.adminUser(token);
+    var admin = Realm.Sync.User.adminUser(token);
 
-  Realm.Sync.addListener(this.url, admin, this.path, 'change', Pop.changeCallback);
-  console.log('Pop app at: ' + this.url + " observing changes at: " + this.path);
+    Realm.Sync.addListener(this.url, admin, this.path, 'change', Pop.changeCallback);
+    console.log('Pop app at: ' + this.url + " observing changes at: " + this.path);
 }
 
 Pop.changeCallback = function(event) {
-	
+
     let realm = event.realm;
 
     let changes = event.changes.Score;
     let indexes = changes.insertions;
 
-	if (indexes.length == 0) return;
-	
+    if (indexes.length == 0) return;
+
     var scores = realm.objects("Score");
 
     for (var i = 0; i < indexes.length; i++) {
-	    let index = indexes[i];
-	    let score = scores[index];
+        let index = indexes[i];
+        let score = scores[index];
 
-	    if (isRealmObject(score)) {
-		    console.log("[score]: " + score.name + " > " + score.time)
-		    
-		    Board.addScore(score, function() {
-			    realm.write(function() {
-		            realm.delete(score)
-		        })			    
-		    })
-		}
-	}
+        if (isRealmObject(score)) {
+            console.log("[score]: " + score.name + " > " + score.time)
+
+            Board.addScore(score, function() {
+                           realm.write(function() {
+                                       realm.delete(score)
+                                       })
+                           })
+        }
+    }
 }
 
 /**
@@ -68,16 +68,16 @@ Pop.changeCallback = function(event) {
  */
 
 var Board = {
-	file: 'board.txt'
+file: 'board.txt'
 }
 
 Board.addScore = function(score, success) {
     fs.appendFile(this.file, score.name+"\t"+score.time+"\n", function (err) {
-		if (err) {
-			return console.error(err);
-		}
-		success()
-	});
+                  if (err) {
+                  return console.error(err);
+                  }
+                  success()
+                  });
 }
 
 //
