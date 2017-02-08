f9dceb78b03f6aac336d8e5a844e7832b9130c23
ericmaxwell2003 <ericmaxwell2003@yahoo.com>
2017-02-08 14:49:18 -0500

    Add keystore for building a signed APK

--- Android/app/src/main/java/realm/io/realmpop/controller/BaseActivity.java
+++ Android/app/src/main/java/realm/io/realmpop/controller/BaseActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-public class BaseActivity extends AppCompatActivity {
+abstract public class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void attachBaseContext(Context newBase) {
