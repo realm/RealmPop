36d325b76f06b472f757177f72f309cedbeb8f4f
ericmaxwell2003 <ericmaxwell2003@yahoo.com>
2017-02-23 09:27:57 -0500

    Add trace level realm logging and an indicator when loading, which IP you are connecting to.

--- Android/app/src/main/java/realm/io/realmpop/RealmPopApplication.java
+++ Android/app/src/main/java/realm/io/realmpop/RealmPopApplication.java
@@ -3,6 +3,8 @@ package realm.io.realmpop;
 import android.app.Application;
 
 import io.realm.Realm;
+import io.realm.log.LogLevel;
+import io.realm.log.RealmLog;
 import realm.io.realmpop.util.SharedPrefsUtils;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
@@ -12,6 +14,7 @@ public class RealmPopApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Realm.init(this);
+        RealmLog.setLevel(LogLevel.TRACE);
         SharedPrefsUtils.init(this);
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
                 .setDefaultFontPath("fonts/PressStart2P.ttf")
--- Android/app/src/main/java/realm/io/realmpop/controller/BaseActivity.java
+++ Android/app/src/main/java/realm/io/realmpop/controller/BaseActivity.java
@@ -3,6 +3,7 @@ package realm.io.realmpop.controller;
 
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import io.realm.Realm;
 import io.realm.SyncConfiguration;
@@ -19,6 +20,7 @@ abstract public class BaseActivity extends AppCompatActivity {
     }
 
     public static void setActiveUser(SyncUser user) {
+        Log.d(BaseActivity.class.getName(), "Connecting to Sync Server at : ["  + REALM_URL.replaceAll("~", user.getIdentity()) + "]");
         final SyncConfiguration syncConfiguration = new SyncConfiguration.Builder(user, REALM_URL).schemaVersion(1).build();
         Realm.setDefaultConfiguration(syncConfiguration);
     }
